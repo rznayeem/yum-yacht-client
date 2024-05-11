85f01d8dd35dec962840bff53f0a295f51915e09
rznayeem <riyazulnayeem@gmail.com>
2024-05-12 02:16:46 +0600

    created initial modal on gallery section

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -53,6 +53,158 @@ const Gallery = () => {
             </p>
 
             {/* modal starts here */}
+            <div className="max-w-96 flex justify-center mx-auto">
+              <button
+                onClick={() => {
+                  user ? setOpenModal(true) : navigate('/login');
+                }}
+                className="group relative flex w-full items-center rounded-lg border-2 border-[#FF923E] p-4 text-[#FF923E]"
+              >
+                <span>Add your thoughts with us</span>
+                <span className="absolute right-3 box-content flex w-1/6 justify-center rounded-md bg-[#FF923E] duration-300 group-hover:w-[94%]">
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    className="w-10"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g strokeWidth="0"></g>
+                    <g strokeLinecap="round" strokeLinejoin="round"></g>
+                    <g>
+                      <path
+                        d="M4 12H20M20 12L14 6M20 12L14 18"
+                        stroke="#fff"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                    </g>
+                  </svg>
+                </span>
+              </button>
+              <div
+                onClick={() => setOpenModal(false)}
+                className={`fixed z-[100] flex items-center justify-center ${
+                  openModal ? 'opacity-1 visible' : 'invisible opacity-0'
+                } inset-0 h-full w-full bg-black/20 backdrop-blur-sm duration-100`}
+              >
+                <div
+                  onClick={e_ => e_.stopPropagation()}
+                  className={`absolute w-full rounded-lg bg-white dark:bg-gray-900 drop-shadow-2xl sm:w-[500px] ${
+                    openModal
+                      ? 'opacity-1 translate-y-0 duration-300'
+                      : '-translate-y-20 opacity-0 duration-150'
+                  }`}
+                >
+                  <form className="px-5 pb-5 pt-3 lg:pb-10 lg:pt-5 lg:px-10">
+                    <svg
+                      onClick={() => setOpenModal(false)}
+                      className="mx-auto mr-0 w-10 cursor-pointer fill-black dark:fill-white"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        <path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z"></path>
+                      </g>
+                    </svg>
+                    <h1 className="pb-8 text-4xl backdrop-blur-sm">Login</h1>
+                    <div className="space-y-5">
+                      <label
+                        htmlFor="email_navigate_ui_modal"
+                        className="block"
+                      >
+                        Email
+                      </label>
+                      <div className="relative">
+                        <input
+                          id="email_navigate_ui_modal"
+                          type="email"
+                          placeholder="example@gmail.com"
+                          className="block w-full rounded-lg p-3 pl-10 outline-none drop-shadow-lg bg-white dark:bg-gray-700 dark:text-white"
+                        />
+                        <span className="absolute left-2 top-1/4">
+                          <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            className="inline-block w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <g strokeWidth="0"></g>
+                            <g
+                              id="SVGRepo_tracerCarrier"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            ></g>
+                            <g id="SVGRepo_iconCarrier">
+                              <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M2 10C2 6.22876 2 4.34315 3.17157 3.17157C4.34315 2 6.22876 2 10 2H14C17.7712 2 19.6569 2 20.8284 3.17157C22 4.34315 22 6.22876 22 10V14C22 17.7712 22 19.6569 20.8284 20.8284C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V10ZM7.73867 16.4465C8.96118 15.5085 10.4591 15 12 15C13.5409 15 15.0388 15.5085 16.2613 16.4465C17.4838 17.3846 18.3627 18.6998 18.7615 20.1883C18.9044 20.7217 18.5878 21.2701 18.0544 21.413C17.5209 21.556 16.9726 21.2394 16.8296 20.7059C16.5448 19.6427 15.917 18.7033 15.0438 18.0332C14.1706 17.3632 13.1007 17 12 17C10.8993 17 9.82942 17.3632 8.95619 18.0332C8.08297 18.7033 7.45525 19.6427 7.17037 20.7059C7.02743 21.2394 6.47909 21.556 5.94563 21.413C5.41216 21.2701 5.09558 20.7217 5.23852 20.1883C5.63734 18.6998 6.51616 17.3846 7.73867 16.4465ZM10 9C10 7.89543 10.8954 7 12 7C13.1046 7 14 7.89543 14 9C14 10.1046 13.1046 11 12 11C10.8954 11 10 10.1046 10 9ZM12 5C9.79086 5 8 6.79086 8 9C8 11.2091 9.79086 13 12 13C14.2091 13 16 11.2091 16 9C16 6.79086 14.2091 5 12 5Z"
+                                className="fill-black dark:fill-white"
+                              ></path>
+                              <rect
+                                x="2.5"
+                                y="2.5"
+                                width="19"
+                                height="19"
+                                rx="3.5"
+                                className="stroke-black dark:stroke-white"
+                              ></rect>
+                            </g>
+                          </svg>
+                        </span>
+                      </div>
+                      <label
+                        htmlFor="password_navigate_ui_modal"
+                        className="block"
+                      >
+                        Password
+                      </label>
+                      <div className="relative">
+                        <input
+                          id="password_navigate_ui_modal"
+                          type="password"
+                          placeholder=".............."
+                          className="block w-full rounded-lg p-3 pl-10 outline-none drop-shadow-lg bg-white dark:bg-gray-700 dark:text-white"
+                        />
+                        <span className="absolute left-2 top-1/4">
+                          <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            className="inline-block w-6 fill-black dark:fill-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <g strokeWidth="0"></g>
+                            <g
+                              id="SVGRepo_tracerCarrier"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            ></g>
+                            <g id="SVGRepo_iconCarrier">
+                              <path d="M20.9098 11.1203V6.73031C20.9098 5.91031 20.2898 4.98031 19.5198 4.67031L13.9498 2.39031C12.6998 1.88031 11.2898 1.88031 10.0398 2.39031L4.46984 4.67031C3.70984 4.98031 3.08984 5.91031 3.08984 6.73031V11.1203C3.08984 16.0103 6.63984 20.5903 11.4898 21.9303C11.8198 22.0203 12.1798 22.0203 12.5098 21.9303C17.3598 20.5903 20.9098 16.0103 20.9098 11.1203ZM12.7498 12.8703V15.5003C12.7498 15.9103 12.4098 16.2503 11.9998 16.2503C11.5898 16.2503 11.2498 15.9103 11.2498 15.5003V12.8703C10.2398 12.5503 9.49984 11.6103 9.49984 10.5003C9.49984 9.12031 10.6198 8.00031 11.9998 8.00031C13.3798 8.00031 14.4998 9.12031 14.4998 10.5003C14.4998 11.6203 13.7598 12.5503 12.7498 12.8703Z"></path>
+                            </g>
+                          </svg>
+                        </span>
+                      </div>
+                    </div>
+                    {/* button type will be submit for handling form submission*/}
+                    <button
+                      type="button"
+                      className="relative py-2.5 px-5 rounded-lg mt-6 bg-white drop-shadow-lg hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-800"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="max-w-screen-xl mx-auto grid gap-8 py-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
