d964b7eec0a9d44f6f3d5ff3d403200519ab7c40
rznayeem <riyazulnayeem@gmail.com>
2024-05-14 04:08:37 +0600

    added newsletter section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from '../../components/Banner/Banner';
+import Newsletter from '../../components/Newsletter/Newsletter';
 import Testimonial from '../../components/Testimonial/Testimonial';
 import TopFoods from '../../components/TopFoods/TopFoods';
 
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner></Banner>
       <TopFoods></TopFoods>
       <Testimonial></Testimonial>
+      <Newsletter></Newsletter>
     </div>
   );
 };
